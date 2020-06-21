dd173a1a5713ab1a8883d75cecd22df08dc0b9a9
Tanya Hodges <tanyac52003@gmail.com>
2020-06-21 21:05:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,14 @@ class Transfer
   end 
   
   def execute_transaction
-     
+    if @sender.valid? && @sender.balance > self.amount && @status == "pending"
+      @ sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
   end 
   
   def reverse_transfer
