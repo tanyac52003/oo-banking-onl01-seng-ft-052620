b391fea25f520395748db83006061745d0219264
Tanya Hodges <tanyac52003@gmail.com>
2020-06-21 21:02:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount, :count 
+   
+  
+  def initialize(sender,receiver,amount)
+   @sender = sender
+   @receiver = receiver
+   @status = "pending"
+   @amount = amount 
+  end 
+  
+  def valid?
+    if sender.valid? && receiver.valid?
+      true 
+    else 
+      false 
+    end 
+  end 
+  
+  def execute_transaction
+     
+  end 
+  
+  def reverse_transfer
+    
+  end 
 end
